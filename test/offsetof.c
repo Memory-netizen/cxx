fb2cff320feb42618d3c388d415166cc6e72b713
memory <282894765@qq.com>
2026-09-09 15:20:44 +0800

    Add offsetof

---
+++ test/offsetof.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+#include "test.h"
+
+typedef struct {
+    int a;
+    char b;
+    int c;
+    double d;
+} T;
+
+int main() {
+    ASSERT(0, offsetof(T, a));
+    ASSERT(4, offsetof(T, b));
+    ASSERT(8, offsetof(T, c));
+    ASSERT(16, offsetof(T, d));
+
+    printf("OK\n");
+    return 0;
+}
